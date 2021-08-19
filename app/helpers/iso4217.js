32b651a49283a5960157c09560bceed3071f88e6
KaMeHb-UA <marlock@bassteam.ml>
2021-08-19 23:50:36 +0300

    fix codes

--- app/helpers/iso4217.js
+++ app/helpers/iso4217.js
@@ -2,4 +2,4 @@ import currencies, { utils } from 'iso4217';
 
 export const { getByCode } = utils;
 
-export default code => currencies[code];
+export default code => currencies[`${code}`.padStart(3, '0')];
