0290de057e3baede215d3f385e187919dc76d555
KaMeHb-UA <marlock@bassteam.ml>
2021-08-21 00:30:46 +0300

    register balance method

--- app/components/methods.js
+++ app/components/methods.js
@@ -1,11 +1,13 @@
-import currencies_rate from './currencies_rate.js'
-import currency_info from './currency_info.js'
-import register from './register.js'
-import transactions from './transactions.js'
+import register from './register.js';
+import balance from './balance.js';
+import currencies_rate from './currencies_rate.js';
+import currency_info from './currency_info.js';
+import transactions from './transactions.js';
 
 export {
     register,
     currencies_rate,
     currency_info,
     transactions,
+    balance,
 }
