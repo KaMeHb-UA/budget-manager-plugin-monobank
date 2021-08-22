d13a693b8ad58b913fa50791cec388eacc250514
KaMeHb-UA <marlock@bassteam.ml>
2021-08-22 18:21:45 +0300

    update interval caller system

--- app/helpers/interval.js
+++ app/helpers/interval.js
@@ -1,9 +1,6 @@
 /**
  * Calls underlying function and returns its result.  
  * If interval after last call hasn't been finished yet, returns last result
- * @template {any[]} T
- * @template R
- * @template {(...args: T) => Promise<R>} F
  */
 export default class{
     #interval
@@ -11,19 +8,12 @@ export default class{
     #last
     #initialized
     #intervalRunning
-    /**
-     * @arg {F} fn
-     * @arg {number} interval
-     */
     constructor(fn, interval){
         this.#fn = fn;
         this.#interval = interval;
+        return (...args) => this.#call(...args);
     }
-    /**
-     * @arg {T} args
-     * @return {Promise<R>}
-     */
-    async call(...args){
+    async #call(...args){
         if(this.#intervalRunning && this.#initialized) return this.#last;
         this.#intervalRunning = true;
         this.#initialized = true;
