702cef707de83ce1e773df51d4bd5fd41d99f438
KaMeHb-UA <marlock@bassteam.ml>
2021-08-22 17:46:51 +0300

    fix currency code, add account id to response

--- app/components/transactions.js
+++ app/components/transactions.js
@@ -13,13 +13,14 @@ const caller = new Interval(async () => {
         return {
             description: tx.description,
             id: tx.id,
-            currency: currency.Code,
+            currency: currencyCode,
             amount: tx.amount / divider,
             time: tx.time * 1000,
             comission: tx.commissionRate / divider,
             cashback: tx.cashbackAmount / divider,
             restBalance: tx.balance / divider,
             mcc: tx.mcc,
+            accountId: tx.account.id,
         };
     });
 }, requestInterval);
